4b49420c9f5f9584a79d538a904ba1a1ae261d32
royfuwei <royfuwei@gmail.com>
2025-03-28 09:33:37 +0800

    chore: add comments for import module-alias/register

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+/**
+ * module-alias/register is used to register the module alias in the project.
+ * This is used to avoid the relative path hell in the project.
+ * For example, instead of using `import DemoUtils from '../../../utils/demo.utils';`,
+ * we can use `import DemoUtils from '@/utils/demo.utils';`.
+ * The module alias is defined in the tsconfig.json file.
+ *
+ * For tsc build, we need to use the `tsc-alias` package to resolve the module alias.
+ */
 import 'module-alias/register';
 import DemoUtils from '@/utils/demo.utils';
-import { configs } from './configs';
+import { configs } from '@/configs';
 
 const main = async () => {
   const demoValue = DemoUtils.getDemoValue();
